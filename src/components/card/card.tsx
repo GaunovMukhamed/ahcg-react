061572aad064aa1d1e9559b333f90c1e7aadab3b
SPACERIDER <spacerider@example.com>
2024-06-03 23:22:42 +0300

    update card component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, SyntheticEvent, useState } from "react";
+import { CSSProperties, ReactNode, SyntheticEvent, useState } from "react";
 import './card.scss';
 import { Image } from 'primereact/image';
 
@@ -15,15 +15,33 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ frontImg, backImg, className, style, turnable = true, enabled = true, onClick }) => {
 
   const [characterCardFlipped, setCharacterCardFlipped] = useState<Boolean>(false);
+  const [cardFocused, setcardFocused] = useState<Boolean>(false);
 
   const onCardClick = (event: any) => {
-    if(event.target.tagName === 'IMG') {
-      if(turnable) {
-        setCharacterCardFlipped(!characterCardFlipped);
-      } else {
-        if(onClick) onClick(event);
-      }
-    }
+    if(event.target.tagName === 'IMG' && onClick) onClick(event);
+  }
+
+  const imageContent = (url: string): ReactNode => {
+    return <div
+      className="relative w-full h-full" 
+      onMouseEnter={() => setcardFocused(true)}
+      onMouseLeave={() => setcardFocused(false)}>
+      <Image
+        src={url}
+        width={'100%'}
+        height={'100%'}
+        alt="character-card"
+        preview
+      />
+      {turnable ? 
+        <div
+          className="flex justify-content-center align-items-center card-button"
+          style={{ top: 'calc(10px + 2rem)', opacity: cardFocused?'1':'0' }}
+          onClick={() => { setCharacterCardFlipped(!characterCardFlipped); setcardFocused(false) }}>
+          <i className="pi pi-refresh" style={{ fontSize: '1.3rem' }}></i>
+        </div>
+      : ''}
+    </div>
   }
 
   return(
@@ -35,22 +53,10 @@ const Card: React.FC<CardProps> = ({ frontImg, backImg, className, style, turnab
         ${className??''}`}
       onClick={onCardClick}>
       <div className='w-full h-full player-card-front border-round'>
-        <Image
-          src={frontImg}
-          width={'100%'}
-          height={'100%'}
-          alt="character-card"
-          preview
-          />
+        {imageContent(frontImg)}
       </div>
       <div className='w-full h-full player-card-back'>
-        <Image
-          src={backImg}
-          width={'100%'}
-          height={'100%'}
-          alt="character-card"
-          preview
-          />
+        {imageContent(backImg)}
       </div>
     </div>
   )
