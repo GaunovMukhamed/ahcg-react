6b484bebbf6ce2e7c0aa62fddae5748cd9a1c409
SPACERIDER <spacerider@example.com>
2024-06-05 22:53:06 +0300

    add counters

--- src/components/token/token.tsx
+++ src/components/token/token.tsx
@@ -22,7 +22,13 @@ const Token: React.FC<TokenProps> = ({ imgUrl, className = '', style }) => {
         width={'100%'}
         height={'100%'}
         alt="token"/>
-      <div className='absolute top-0 left-0 w-full h-full flex justify-content-center align-items-center z-1'>{count}</div>
+      <div className='
+        absolute top-0 left-0 w-full h-full
+        flex justify-content-center align-items-center z-1'>
+          <div
+            style={{ padding: '0.15rem', paddingBottom: '0.2rem', lineHeight: '0.7' }}
+            className='surface-ground w-min h-min border-1 border-primary  border-round text-yellow-500 flex justify-content-center align-items-center font-bold'>{count}</div>
+        </div>
     </div>
   )
 }
--- src/pages/game-page/components/game-scene/components/character-panel.tsx
+++ src/pages/game-page/components/game-scene/components/character-panel.tsx
@@ -12,20 +12,20 @@ const CharacterPanel: React.FC<any> = () => {
   const characterInfo = useAppSelector((state) => state.general.players[login]);
 
   return(
-    <div className='relative pt-2 border-top-1 border-primary flex overflow-visible z-1'>
-      <Card
-        className='w-14rem h-10rem'
-        frontImg={characterInfo.character?.frontImg??''}
-        backImg={characterInfo.character?.backImg??''} />  
-      <div className='h-full ml-2 flex flex-column justify-content-between'>
-        <Token style={{ width: '2rem', height: '1.9rem' }} imgUrl='https://drive.google.com/thumbnail?id=1an806GsJK7Dot6d2jOIL7N2OUyzLoT6w&sz=w1000' />
-        <Token style={{ width: '2rem', height: '1.9rem' }} imgUrl='https://drive.google.com/thumbnail?id=1aXsOh4oKiugZn288lSAFrSPkK_0YQxeq&sz=w1000' />
-        <Token className='w-2rem h-2rem' imgUrl='https://drive.google.com/thumbnail?id=1aSteZDhEqZj2jEOQtD6rhNtj6NhgujBI&sz=w1000' />
-        <Token className='w-2rem h-2rem' imgUrl='https://drive.google.com/thumbnail?id=1aU8GwFcHLAAzekpKxc-YBPkxSJsM4AWZ&sz=w1000' />
+    <div className='relative h-min pt-2 border-top-1 border-primary flex overflow-visible z-1'>
+      <div className='w-14rem h-min flex flex-column mr-2 justify-content-between align-items-center'>
+        <div className='w-full mb-2 flex justify-content-between'>
+          <Token className='w-3rem h-3rem' imgUrl='https://drive.google.com/thumbnail?id=1aU8GwFcHLAAzekpKxc-YBPkxSJsM4AWZ&sz=w1000' />
+          <Token className='w-3rem h-3rem' imgUrl='https://drive.google.com/thumbnail?id=1aSteZDhEqZj2jEOQtD6rhNtj6NhgujBI&sz=w1000' />
+          <Token style={{ width: '3rem', height: '2.9rem' }} imgUrl='https://drive.google.com/thumbnail?id=1aXsOh4oKiugZn288lSAFrSPkK_0YQxeq&sz=w1000' />
+          <Token style={{ width: '3rem', height: '2.9rem' }} imgUrl='https://drive.google.com/thumbnail?id=1an806GsJK7Dot6d2jOIL7N2OUyzLoT6w&sz=w1000' />
+        </div>
+        <Card
+          className='w-13rem h-10rem'
+          frontImg={characterInfo.character?.frontImg??''}
+          backImg={characterInfo.character?.backImg??''} />  
       </div>
-      <div
-        style={{ width: 'calc(100% - 14rem - 3.5rem)', bottom: '0', right: '0.5rem' }}
-        className='absolute flex align-items-center'>
+      <div className='flex-1 flex align-items-center'>
         <div className='flex-1 flex flex-column mr-2'>
           <CardLine className='mb-2 h-6rem'/> 
           <CardLine className='h-6rem' /> 
