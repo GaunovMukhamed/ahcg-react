6251db4667a8fd14d82e721361ae44f7dbe5387c
SPACERIDER <spacerider@example.com>
2024-07-13 21:44:09 +0300

    update game page

---
+++ src/pages/game-page/components/game-scene/components/scene-panel.tsx
@@ -0,0 +1,7 @@
+const ScenePanel: React.FC<any> = () => {
+  return(
+    <div className="sticky top-0 left-0">scene panel</div>
+  )
+}
+
+export {ScenePanel};
--- src/pages/game-page/components/game-scene/game-scene.tsx
+++ src/pages/game-page/components/game-scene/game-scene.tsx
@@ -1,9 +1,11 @@
 import { CharacterPanel } from "./components/character-panel";
+import { ScenePanel } from "./components/scene-panel";
         
 const GameScene: React.FC<any> = () => {
   return(
-    <div className="w-full h-full flex flex-column justify-content-end overflow-hidden">
+    <div className="relative w-full h-full flex flex-column justify-content-end overflow-hidden">
       <div className="w-full h-full mb-2 flex-1 flex flex-column">
+        <ScenePanel />
         <div className="flex-1">scene</div>
       </div>
       <CharacterPanel />
